3e736a3b17f6267e8d9bf22f47f7f6d824abb154
iver <iverflores6@gmail.com>
2022-07-19 09:29:59 -0400

    nueva version

---
+++ app/src/test/java/com/ip/citasmedicas/dialogs/DialogFragmentAccountAdministradorTest.java
@@ -0,0 +1,19 @@
+package com.ip.citasmedicas.dialogs;
+
+
+import org.junit.Test;
+
+public class DialogFragmentAccountAdministradorTest {
+
+    @Test
+    public void onCreateDialog() {
+    }
+
+    @Test
+    public void onClick() {
+    }
+
+    @Test
+    public void onActivityResult() {
+    }
+}
